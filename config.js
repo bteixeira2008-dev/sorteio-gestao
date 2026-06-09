e3e7526d08ffb02d4867acd9a201c9147ea1bd15
bteixeira2008-dev <bteixeira2008@gmail.com>
2026-06-09 21:50:59 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const SORTEIO = {
+  data: "2026-08-31T21:00:00",
+  publicado: false,
+  vencedores: {
+    primeiro: "",
+    segundo: "",
+    terceiro: ""
+  }
+};
+
+// No dia do sorteio, muda para:
+// publicado: true,
+// e preenche os vencedores.
